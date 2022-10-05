b9a84ab2413aa64fc1e52c2bb4a1d14773256ee2
Christian Ullrich <chris@chrullrich.net>
2022-10-05 10:14:17 +0200

    Update both numeric and string patchlevel
    Fixes #284.

--- scripts/patchlevel.sh
+++ scripts/patchlevel.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+PATCHLEVEL_STR=`echo $PATCHLEVEL | sed "s/^0*\\([^0].*\\)/\\1/"`
+sed -i "s/# *define VIM_VERSION_PATCHLEVEL[^_].*/#define VIM_VERSION_PATCHLEVEL ${PATCHLEVEL_STR}\n/" ./version.h
 sed -i "s/#define VIM_VERSION_PATCHLEVEL_STR.*/#define VIM_VERSION_PATCHLEVEL_STR \"$PATCHLEVEL\"\n/" ./version.h
