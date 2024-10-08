317ab437169fe13b74b7862e6ce5f0a4bf004d40
K.Takata <kentkt@csc.jp>
2024-10-08 16:34:18 +0900

    Update gettext-iconv-windows to v0.22.5a-v1.17 (#355)
    * Update gettext-iconv-windows to v0.22.5a-v1.17
    
    https://github.com/mlocati/gettext-iconv-windows/releases/tag/v0.22.5a-v1.17
    
    * package: Include libgcc_s_sjlj-1.dll again
    
    This automatically detect whether libgcc_s_sjlj-1.dll is needed.
    If it is needed, makensis is called with the /DINCLUDE_LIBGCC=1 option.
    
    This requires https://github.com/vim/vim/pull/15819 .

--- appveyor.bat
+++ appveyor.bat
@@ -80,8 +80,8 @@ set "TCL_DIR=%DEPENDENCIES%\Tcl"
 set "TCL_DLL=tcl%TCL_VER%t.dll"
 set "TCL_LIBRARY=%TCL_DIR%\lib\tcl%TCL_VER_LONG%"
 :: Gettext
-set "GETTEXT32_URL=https://github.com/mlocati/gettext-iconv-windows/releases/download/v0.21-v1.16/gettext0.21-iconv1.16-shared-32.zip"
-set "GETTEXT64_URL=https://github.com/mlocati/gettext-iconv-windows/releases/download/v0.21-v1.16/gettext0.21-iconv1.16-shared-64.zip"
+set "GETTEXT32_URL=https://github.com/mlocati/gettext-iconv-windows/releases/download/v0.22.5a-v1.17/gettext0.22.5a-iconv1.17-shared-32.zip"
+set "GETTEXT64_URL=https://github.com/mlocati/gettext-iconv-windows/releases/download/v0.22.5a-v1.17/gettext0.22.5a-iconv1.17-shared-64.zip"
 :: winpty
 set "WINPTY_URL=https://github.com/rprichard/winpty/releases/download/0.4.3/winpty-0.4.3-msvc2015.zip"
 :: UPX
@@ -284,6 +284,14 @@ goto :eof
 @echo on
 cd vim\src
 
+:: Check if we need to copy libgcc_s_sjlj-1.dll.
+%CYGWIN_DIR%\bin\bash -lc "objdump -p $(cygpath '%DEPENDENCIES%\gettext32')/libintl-8.dll | grep -q 'libgcc_s_sjlj-1\.dll'"
+if ERRORLEVEL 1 (
+	set INCLUDE_LIBGCC=0
+) else (
+	set INCLUDE_LIBGCC=1
+)
+
 mkdir GvimExt64
 mkdir GvimExt32
 :: Build both 64- and 32-bit versions of gvimext.dll for the installer
@@ -321,13 +329,15 @@ copy /Y %DEPENDENCIES%\gettext64\libintl-8.dll       ..\runtime\GvimExt64\
 copy /Y GvimExt32\*.*                    ..\runtime\GvimExt32\
 copy /Y %DEPENDENCIES%\gettext32\libiconv-2.dll      ..\runtime\GvimExt32\
 copy /Y %DEPENDENCIES%\gettext32\libintl-8.dll       ..\runtime\GvimExt32\
-@rem copy /Y c:\gettext32\libgcc_s_sjlj-1.dll ..\runtime\GvimExt32\
-@rem libgcc_s_sjlj-1.dll is not needed anymore. Don't include it in the installer.
-@rem if exist %DEPENDENCIES%\gettext32\libgcc_s_sjlj-1.dll del %DEPENDENCIES%\gettext32\libgcc_s_sjlj-1.dll
+if "%INCLUDE_LIBGCC%"=="1" (
+	copy /Y %DEPENDENCIES%\gettext32\libgcc_s_sjlj-1.dll ..\runtime\GvimExt32\
+)
 copy /Y ..\..\diff.exe ..\runtime\
 copy /Y %DEPENDENCIES%\gettext%BIT%\libiconv-2.dll   ..\runtime\
 copy /Y %DEPENDENCIES%\gettext%BIT%\libintl-8.dll    ..\runtime\
-@rem if exist c:\gettext%BIT%\libgcc_s_sjlj-1.dll copy /Y c:\gettext%BIT%\libgcc_s_sjlj-1.dll ..\runtime\
+if "%INCLUDE_LIBGCC%-%BIT%"=="1-32" (
+	copy /Y %DEPENDENCIES%\gettext32\libgcc_s_sjlj-1.dll ..\runtime\
+)
 copy /Y winpty* ..\runtime\
 copy /Y winpty* ..\..\
 set "dir=vim%MAJOR%%MINOR%"
@@ -356,10 +366,11 @@ pushd ..\nsis
 
 7z.exe x -y icons.zip > nul
 if /I "%ARCH%"=="x64" (
-    "%ProgramFiles(x86)%\NSIS\makensis.exe" /INPUTCHARSET UTF8 /DVIMRT=..\runtime /DGETTEXT=%DEPENDENCIES% /DWIN64=1 /DPATCHLEVEL=%PATCHLEVEL% gvim.nsi "/XOutFile ..\..\gvim_%VER_NUM%_%ARCH%.exe"
+	set WIN64=1
 ) else (
-    "%ProgramFiles(x86)%\NSIS\makensis.exe" /INPUTCHARSET UTF8 /DVIMRT=..\runtime /DGETTEXT=%DEPENDENCIES% /DPATCHLEVEL=%PATCHLEVEL% gvim.nsi "/XOutFile ..\..\gvim_%VER_NUM%_%ARCH%.exe"
+	set WIN64=0
 )
+"%ProgramFiles(x86)%\NSIS\makensis.exe" /INPUTCHARSET UTF8 /DVIMRT=..\runtime /DGETTEXT=%DEPENDENCIES% /DWIN64=%WIN64% /DINCLUDE_LIBGCC=%INCLUDE_LIBGCC% /DPATCHLEVEL=%PATCHLEVEL% gvim.nsi "/XOutFile ..\..\gvim_%VER_NUM%_%ARCH%.exe"
 popd
 
 :: Create zipfile for signing with signpath.io
