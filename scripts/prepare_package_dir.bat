49f5e731817f1a4c75cbc04770d7282f10cd1f40
K.Takata <kentkt@csc.jp>
2026-06-17 23:57:33 +0900

    Use "exit /b 1"

--- scripts/prepare_package_dir.bat
+++ scripts/prepare_package_dir.bat
@@ -18,25 +18,25 @@ md "%DEST%"
 robocopy "%VIMSRC%\runtime" "%DEST%" /E /NP /NFL /NDL
 if ERRORLEVEL 8 exit /b 1
 
-copy "%VIMSRC%\src\*vim*.exe" "%DEST%" || exit 1
-copy "%VIMSRC%\src\vim*.dll"  "%DEST%" || exit 1
-copy "%VIMSRC%\src\tee\*.exe" "%DEST%" || exit 1
-copy "%VIMSRC%\src\xxd\*.exe" "%DEST%" || exit 1
-copy "%VIMSRC%\src\libsodium.dll" "%DEST%" || exit 1
-copy "%VIMSRC%\src\winpty*"   "%DEST%" || exit 1
-copy "%VIMSRC%\README.txt"    "%DEST%" || exit 1
-copy "%VIMSRC%\LICENSE"       "%DEST%\LICENSE.txt" || exit 1
-copy "%VIMSRC%\uninstall.txt" "%DEST%" || exit 1
-copy "%VIMSRC%\vimtutor.bat"  "%DEST%" || exit 1
-copy "%VIMSRC%\src\vim.ico"   "%DEST%" || exit 1
-copy "%VIMSRC%\src\install.exe" "%DEST%" || exit 1
-copy "%VIMSRC%\src\uninstall.exe" "%DEST%" || exit 1
-copy "%DIFFEXE%\diff.exe"     "%DEST%" || exit 1
-rem copy "%LUA_DIR%\lua*.dll"   "%DEST%" || exit 1
+copy "%VIMSRC%\src\*vim*.exe" "%DEST%" || exit /b 1
+copy "%VIMSRC%\src\vim*.dll"  "%DEST%" || exit /b 1
+copy "%VIMSRC%\src\tee\*.exe" "%DEST%" || exit /b 1
+copy "%VIMSRC%\src\xxd\*.exe" "%DEST%" || exit /b 1
+copy "%VIMSRC%\src\libsodium.dll" "%DEST%" || exit /b 1
+copy "%VIMSRC%\src\winpty*"   "%DEST%" || exit /b 1
+copy "%VIMSRC%\README.txt"    "%DEST%" || exit /b 1
+copy "%VIMSRC%\LICENSE"       "%DEST%\LICENSE.txt" || exit /b 1
+copy "%VIMSRC%\uninstall.txt" "%DEST%" || exit /b 1
+copy "%VIMSRC%\vimtutor.bat"  "%DEST%" || exit /b 1
+copy "%VIMSRC%\src\vim.ico"   "%DEST%" || exit /b 1
+copy "%VIMSRC%\src\install.exe" "%DEST%" || exit /b 1
+copy "%VIMSRC%\src\uninstall.exe" "%DEST%" || exit /b 1
+copy "%DIFFEXE%\diff.exe"     "%DEST%" || exit /b 1
+rem copy "%LUA_DIR%\lua*.dll"   "%DEST%" || exit /b 1
 
 :: Copy gettext for (g)vim.exe
-copy %GETTEXT_DIR%\libiconv-2.dll %DEST% || exit 1
-copy %GETTEXT_DIR%\libintl-8.dll  %DEST% || exit 1
+copy %GETTEXT_DIR%\libiconv-2.dll %DEST% || exit /b 1
+copy %GETTEXT_DIR%\libintl-8.dll  %DEST% || exit /b 1
 :: Check if libgcc_s_*.dll is required
 set INCLUDE_LIBGCC=0
 if NOT "%ARCH%"=="arm64" (
@@ -45,21 +45,21 @@ if NOT "%ARCH%"=="arm64" (
 if "%ARCH%"=="x86" (
   if "%INCLUDE_LIBGCC%" EQU 1 (
     if exist %GETTEXT_DIR%\libgcc_s_sjlj-1.dll (
-      copy %GETTEXT_DIR%\libgcc_s_sjlj-1.dll %DEST% || exit 1
+      copy %GETTEXT_DIR%\libgcc_s_sjlj-1.dll %DEST% || exit /b 1
     )
   )
 )
 
 :: Copy gettext for GvimExt64
-copy %GETTEXT64_DIR%\libiconv-2.dll %DEST%\GvimExt64 || exit 1
-copy %GETTEXT64_DIR%\libintl-8.dll  %DEST%\GvimExt64 || exit 1
+copy %GETTEXT64_DIR%\libiconv-2.dll %DEST%\GvimExt64 || exit /b 1
+copy %GETTEXT64_DIR%\libintl-8.dll  %DEST%\GvimExt64 || exit /b 1
 :: Copy gettext for GvimExt32
 if NOT "%ARCH%"=="arm64" (
-  copy %GETTEXT32_DIR%\libiconv-2.dll %DEST%\GvimExt32 || exit 1
-  copy %GETTEXT32_DIR%\libintl-8.dll  %DEST%\GvimExt32 || exit 1
+  copy %GETTEXT32_DIR%\libiconv-2.dll %DEST%\GvimExt32 || exit /b 1
+  copy %GETTEXT32_DIR%\libintl-8.dll  %DEST%\GvimExt32 || exit /b 1
   if "%INCLUDE_LIBGCC%" EQU 1 (
     if exist %GETTEXT32_DIR%\libgcc_s_sjlj-1.dll (
-      copy %GETTEXT32_DIR%\libgcc_s_sjlj-1.dll %DEST%\GvimExt32 || exit 1
+      copy %GETTEXT32_DIR%\libgcc_s_sjlj-1.dll %DEST%\GvimExt32 || exit /b 1
     )
   )
 )
